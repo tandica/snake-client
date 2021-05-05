43b244e13a800f170b716bdfd9bb9f7f322455c3
tandica <williams.tandica@gmail.com>
2021-05-05 13:12:18 -0400

    added client file with exported modules

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+const connect = function () {
+    const conn = net.createConnection({
+      host: '135.23.222.131',
+      port: 50542
+    });
+    
+    // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  conn.on("connect", () => {
+      console.log('time to play');
+      conn.write('Name: TTW')
+      });
+  
+    return conn;
+  };
+
+  module.exports = { connect };
--- play.js
+++ play.js
@@ -1,21 +1,21 @@
 const net = require("net");
-
+const { connect } = require("./client");
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '135.23.222.131',
+//     port: 50542
+//   });
   
-  // interpret incoming data as text
-conn.setEncoding("utf8");
+//   // interpret incoming data as text
+// conn.setEncoding("utf8");
 
-conn.on("connect", () => {
-    console.log('time to play');
-    });
+// conn.on("connect", () => {
+//     console.log('time to play');
+//     });
 
-  return conn;
-};
+//   return conn;
+// };
 
 
 console.log("Connecting ...");
