b9c5fd925b1bdf8f88a1b020ac3c63e8209f2277
tandica <williams.tandica@gmail.com>
2021-05-05 13:24:33 -0400

    updated commit

--- client.js
+++ client.js
@@ -3,10 +3,9 @@ const net = require("net");
 const connect = function () {
     const conn = net.createConnection({
       host: '135.23.222.131',
-      port: 50542
+      port: 50542,
     });
     
-    // interpret incoming data as text
   conn.setEncoding("utf8");
   
   conn.on("connect", () => {
--- play.js
+++ play.js
@@ -1,13 +1,13 @@
 const net = require("net");
 const { connect } = require("./client");
-// establishes a connection with the game server
+
 // const connect = function () {
 //   const conn = net.createConnection({
 //     host: '135.23.222.131',
 //     port: 50542
 //   });
   
-//   // interpret incoming data as text
+
 // conn.setEncoding("utf8");
 
 // conn.on("connect", () => {
