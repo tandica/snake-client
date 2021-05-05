fdfcfd5ffccfb86627822ce0d03424e65561183f
tandica <williams.tandica@gmail.com>
2021-05-05 13:50:24 -0400

    updated commit

--- play.js
+++ play.js
@@ -1,22 +1,27 @@
 const net = require("net");
 const { connect } = require("./client");
 
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: '135.23.222.131',
-//     port: 50542
-//   });
-  
 
-// conn.setEncoding("utf8");
+const handleUserInput = function (key) {
+    if (key === '\u0003') {
+        process.exit();
+    }
+};
 
-// conn.on("connect", () => {
-//     console.log('time to play');
-//     });
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    //register handleUserInput as the data callback
+    stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
 
-//   return conn;
-// };
+    return stdin;
+  };
 
 
 console.log("Connecting ...");
 connect();
+setupInput();
